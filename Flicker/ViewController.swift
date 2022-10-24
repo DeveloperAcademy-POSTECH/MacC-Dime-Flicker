112519151bc390b5eb25b080a60075aba31c8b00
kimdoyoung <57849386+coby5502@users.noreply.github.com>
2022-10-24 09:06:00 +0900

    Initial Commit

---
+++ Flicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flicker
+//
+//  Created by COBY_PRO on 2022/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
