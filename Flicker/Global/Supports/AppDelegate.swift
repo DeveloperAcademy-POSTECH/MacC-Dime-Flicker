9839935237d3175e269358911f409b4c3c398c7a
Hardydev <dusgh15378@gmail.com>
2022-11-01 11:08:37 +0900

    :zap: 애플로그인 파이어베이스 연동으로 변경

--- Flicker/Global/Supports/AppDelegate.swift
+++ Flicker/Global/Supports/AppDelegate.swift
@@ -18,43 +18,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUser
     var userToken: String = ""
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let AuthController = UINavigationController(rootViewController: LogInViewController())
-        
-        let appleIDProvider = ASAuthorizationAppleIDProvider()
-        appleIDProvider.getCredentialState(forUserID: KeychainItem.currentUserIdentifier) { (credentialState, error) in
-            if let error = error {
-                print(error)
-            }
-            switch credentialState {
-            case .authorized:
-                print("1")
-                break // 이미 로그인을 했으면 넘어가는 로직
-            case .revoked, .notFound:
-                print("2")
-                // 로그인이 되어있지 않으면 로그인뷰로 이동
-                DispatchQueue.main.async {
-                    print("3")
-                    self.window?.rootViewController = AuthController
-                }
-            default:
-                break
-            }
-            print("4")
-        }
+
         FirebaseApp.configure()
-//
-//        Messaging.messaging().delegate = self
-//        UNUserNotificationCenter.current().delegate = self
-//
-//        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { success, _ in
-//            guard success else {
-//                return
-//            }
-//
-//            print("Success in APNS registry")
-//        }
-//
-//        application.registerForRemoteNotifications()
+
+        if let user = FirebaseAuth.Auth.auth().currentUser {
+            print("로그인되어있음", user.uid, user.email ?? "-")
+        }
+
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { success, _ in
+            guard success else {
+                return
+            }
+
+            print("Success in APNS registry")
+        }
+
+        application.registerForRemoteNotifications()
         
         return true
     }
--- Flicker/Screens/Login/LoginViewController.swift
+++ Flicker/Screens/Login/LoginViewController.swift
@@ -10,121 +10,27 @@ import UIKit
 import SnapKit
 import Then
 import AuthenticationServices
+import CryptoKit
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
 
 class LogInViewController: BaseViewController {
+
+    fileprivate var currentNonce: String?
 
     private let appleLoginButton = ASAuthorizationAppleIDButton(type: .signIn, style: .whiteOutline).then {
         $0.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
     }
     
-//    private let loginLabel = UILabel().then {
-//        $0.textAlignment = .center
-//        $0.text = "로그인"
-//        $0.font = .systemFont(ofSize: 24, weight: .semibold)
-//    }
-    
-//    private let emailField = UITextField().then {
-//        let attributes = [
-//            NSAttributedString.Key.foregroundColor : UIColor.mainBlack,
-//            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17, weight: .regular)
-//        ]
-//
-//        $0.backgroundColor = .white
-//        $0.attributedPlaceholder = NSAttributedString(string: "Email", attributes: attributes)
-//        $0.autocapitalizationType = .none
-//        $0.layer.cornerRadius = 12
-//        $0.layer.masksToBounds = true
-//        $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
-//        $0.leftViewMode = .always
-//        $0.clipsToBounds = false
-//        $0.makeShadow(color: .black, opacity: 0.08, offset: CGSize(width: 0, height: 4), radius: 20)
-//    }
-    
-//    private let passwordField = UITextField().then {
-//        let attributes = [
-//            NSAttributedString.Key.foregroundColor : UIColor.mainBlack,
-//            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17, weight: .regular)
-//        ]
-//
-//        $0.backgroundColor = .white
-//        $0.attributedPlaceholder = NSAttributedString(string: "Password", attributes: attributes)
-//        $0.layer.cornerRadius = 12
-//        $0.layer.masksToBounds = true
-//        $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
-//        $0.leftViewMode = .always
-//        $0.clipsToBounds = false
-//        $0.isSecureTextEntry = true
-//        $0.makeShadow(color: .black, opacity: 0.08, offset: CGSize(width: 0, height: 4), radius: 20)
-//    }
-    
-//    private let logInbutton = UIButton().then {
-//        $0.backgroundColor = .mainBlack
-//        $0.setTitleColor(.white, for: .normal)
-//        $0.setTitle("Log In", for: .normal)
-//    }
-//
-//    private let signUpButton = UIButton().then {
-//        $0.backgroundColor = .mainBlack
-//        $0.setTitleColor(.white, for: .normal)
-//        $0.setTitle("Sign Up", for: .normal)
-//    }
-    
-//    private let logInErrorLabel = UILabel().then {
-//        $0.textAlignment = .center
-//        $0.text = "Failed to Log In"
-//        $0.font = .systemFont(ofSize: 24, weight: .semibold)
-//        $0.textColor = .mainBlack
-//    }
-    
     override func render() {
-//        view.addSubviews(loginLabel, emailField, passwordField, logInbutton, signUpButton, logInErrorLabel)
-
         view.addSubview(appleLoginButton)
-
-//        logInbutton.addTarget(self, action: #selector(didTapLogInbutton), for: .touchUpInside)
-//        signUpButton.addTarget(self, action: #selector(didTapSignUpButton), for: .touchUpInside)
-//
-//        logInErrorLabel.isHidden = true
 
         appleLoginButton.snp.makeConstraints {
             $0.center.equalToSuperview()
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.height.equalTo(60)
         }
-
-//        loginLabel.snp.makeConstraints {
-//            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(30)
-//            $0.leading.trailing.equalToSuperview()
-//        }
-//
-//        emailField.snp.makeConstraints {
-//            $0.top.equalTo(loginLabel.snp.bottom).offset(50)
-//            $0.leading.trailing.equalToSuperview().inset(20)
-//            $0.height.equalTo(60)
-//        }
-//
-//        passwordField.snp.makeConstraints {
-//            $0.top.equalTo(emailField.snp.bottom).offset(10)
-//            $0.leading.trailing.equalToSuperview().inset(20)
-//            $0.height.equalTo(60)
-//        }
-//
-//        logInbutton.snp.makeConstraints {
-//            $0.top.equalTo(passwordField.snp.bottom).offset(20)
-//            $0.leading.trailing.equalToSuperview().inset(20)
-//            $0.height.equalTo(60)
-//        }
-//
-//        signUpButton.snp.makeConstraints {
-//            $0.top.equalTo(logInbutton.snp.bottom).offset(10)
-//            $0.leading.trailing.equalToSuperview().inset(20)
-//            $0.height.equalTo(60)
-//        }
-        
-//        logInErrorLabel.snp.makeConstraints {
-//            $0.top.equalTo(signUpButton.snp.bottom).offset(30)
-//            $0.leading.trailing.equalToSuperview().inset(20)
-//        }
     }
     
     override func setupNavigationBar() {
@@ -134,31 +40,7 @@ class LogInViewController: BaseViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-//        emailField.becomeFirstResponder()
     }
-    
-//    @objc private func didTapLogInbutton() {
-//        guard let email = emailField.text, !email.isEmpty,
-//              let password = passwordField.text, !password.isEmpty else {
-//            logInErrorLabel.isHidden = false
-//            print("Missing field data")
-//            return
-//        }
-//
-//        Task { [weak self] in
-//            guard let userId = await FirebaseManager.shared.signInUser(email: email, password: password) else {
-//                self?.logInErrorLabel.isHidden = false
-//                return
-//            }
-//            await FirebaseManager.shared.updateUserToken(uid: userId)
-//            self?.goHome()
-//        }
-//    }
-    
-//    @objc private func didTapSignUpButton() {
-//        let viewController = SignUpViewController()
-//        self.navigationController?.pushViewController(viewController, animated: true)
-//    }
     
     private func goHome() {
         let viewController = TabbarViewController()
@@ -166,13 +48,36 @@ class LogInViewController: BaseViewController {
         navigationController.modalPresentationStyle = .fullScreen
         self.present(navigationController, animated: true, completion: nil)
     }
+
+    @available(iOS 13, *)
+    func createAppleIDRequest() -> ASAuthorizationAppleIDRequest {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        // 애플로그인은 사용자에게서 2가지 정보를 요구함
+        request.requestedScopes = [.fullName, .email]
+
+        let nonce = randomNonceString()
+        request.nonce = sha256(nonce)
+        currentNonce = nonce
+
+        return request
+    }
+
+    @available(iOS 13, *)
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+        }.joined()
+
+        return hashString
+    }
     
     //Apple ID 승인 요청
     @objc private func handleAuthorizationAppleIDButtonPress() {
         let appleIDProvider = ASAuthorizationAppleIDProvider()
-        let request = appleIDProvider.createRequest()
-        //애플에서 제공받을 수 있도록 요청할 수 있는건 이름과 이메일 뿐임
-        request.requestedScopes = [.fullName, .email]
+        let request = createAppleIDRequest()
 
         let authorizationController = ASAuthorizationController(authorizationRequests: [request])
         authorizationController.delegate = self
@@ -191,37 +96,71 @@ extension LogInViewController: ASAuthorizationControllerDelegate, ASAuthorizatio
 
 // 사용자 인증 후 처리
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        switch authorization.credential {
-            //비밀번호 및 페이스ID인증
-        case let appleIDCredential as ASAuthorizationAppleIDCredential:
-            // 앱 내부의 계정에서 사용하는 값
-            let userIdentifier = appleIDCredential.user
-            let fullName = appleIDCredential.fullName
-            let email = appleIDCredential.email
-            saveUserInKeychain(userIdentifier)
-            self.goHome()
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            // 현재 nonce가 설정되어 있는지 확인
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            // ID 토큰값을 검색
+            guard let appleIDtoken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            //문자열로 변환
+            guard let idTokenString = String(data: appleIDtoken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDtoken.debugDescription)")
+                return
+            }
 
-            //iCloud의 패스워드 연동
-        case let passwordCredental as ASPasswordCredential:
-            let username = passwordCredental.user
-            let password = passwordCredental.password
-            self.goHome()
+            let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                      idToken: idTokenString,
+                                                      rawNonce: nonce)
 
-        default:
-            break
+            FirebaseAuth.Auth.auth().signIn(with: credential) { (authDataResult, error) in
+                if let user = authDataResult?.user {
+                    //로그인 성공 시
+                    self.goHome()
+                }
+
+                if error != nil {
+                    print(error?.localizedDescription ?? "error" as Any)
+                    return
+                }
+            }
         }
     }
-// 사용자 인증 실패 시
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        print("인증이 실패 되었습니다.")
-        // TODO: 다시 로그인 뷰가 로딩되어 돌아가는 로직 구현?
+}
+
+//임의로 생성되는 암호화 토큰이다.
+
+private func randomNonceString(length: Int = 32) -> String {
+  precondition(length > 0)
+  let charset: Array<Character> =
+      Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+  var result = ""
+  var remainingLength = length
+
+  while remainingLength > 0 {
+    let randoms: [UInt8] = (0 ..< 16).map { _ in
+      var random: UInt8 = 0
+      let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+      if errorCode != errSecSuccess {
+        fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+      }
+      return random
     }
 
-    private func saveUserInKeychain(_ userIdentifier: String) {
-        do {
-            try KeychainItem(service: "com.dime.Flicker", account: "userIdentifier").saveItem(userIdentifier)
-        } catch {
-            print("Unable to save userIdentifier to keychain.")
-        }
+    randoms.forEach { random in
+      if remainingLength == 0 {
+        return
+      }
+
+      if random < charset.count {
+        result.append(charset[Int(random)])
+        remainingLength -= 1
+      }
     }
+  }
+
+  return result
 }
