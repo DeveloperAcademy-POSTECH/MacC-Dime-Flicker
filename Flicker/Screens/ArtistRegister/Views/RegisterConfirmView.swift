8b34e3cdf0f877fd9594b38e79df8cb5a28125f7
PecanPie <hidra0321@naver.com>
2022-11-01 20:05:55 +0900

    :tada: Started Project and created folders and .swift
    Just a start.

---
+++ Flicker/Screens/ArtistRegister/Views/RegisterConfirmView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterConfirmView.swift
+//  Flicker
+//
+//  Created by KYUBO A. SHIM on 2022/11/01.
+//
+
+import UIKit
+
+class RegisterConfirmView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Flicker/Screens/ArtistRegister/Views/RegisterRegionView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterRegionView.swift
+//  Flicker
+//
+//  Created by KYUBO A. SHIM on 2022/11/01.
+//
+
+import UIKit
+
+class RegisterRegionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
