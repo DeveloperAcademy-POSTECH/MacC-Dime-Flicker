9bfb5d809467e04f3090738c04bdfc669f177fe1
PecanPie <hidra0321@naver.com>
2022-11-01 20:05:55 +0900

    :tada: Started Project and created folders and .swift
    Just a start.

---
+++ Flicker/Screens/ArtistRegister/Views/RegisterPortfolioView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterPortfolioView.swift
+//  Flicker
+//
+//  Created by KYUBO A. SHIM on 2022/11/01.
+//
+
+import UIKit
+
+class RegisterPortfolioView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Flicker/Screens/ArtistRegister/Views/RegisterWelcomView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterWelcomView.swift
+//  Flicker
+//
+//  Created by KYUBO A. SHIM on 2022/11/01.
+//
+
+import UIKit
+
+class RegisterWelcomView: UIView {
+
+    /*
+     Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
