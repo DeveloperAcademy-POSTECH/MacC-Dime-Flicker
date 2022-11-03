d9f1e45f96db1c2a88597b712be5bfd85f230f4b
coby5502 <57849386+coby5502@users.noreply.github.com>
2022-11-03 11:08:15 +0900

    💄 지역 리스트 구현

--- Flicker/Screens/Main/MainViewController.swift
+++ Flicker/Screens/Main/MainViewController.swift
@@ -11,18 +11,42 @@ import SnapKit
 import Then
 
 final class MainViewController: BaseViewController {
+    
+    private var region: String = "전체"
 
     // MARK: - property
     
-    private let screenText = UILabel().then {
-        $0.textColor = .red
-        $0.text = "홈"
+    private let appTitleLabel = UILabel().then {
+        $0.font = UIFont(name: "TsukimiRounded-Bold", size: 30)
+        $0.textColor = .mainPink
+        $0.textAlignment = .center
+        $0.text = "SHUGGLE"
+    }
+    
+    private let regionListHorizontalView = RegionListHorizontalView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        regionListHorizontalView.setParentViewController(viewController: self)
     }
     
     override func render() {
-        view.addSubview(screenText)
-        screenText.snp.makeConstraints {
-            $0.centerX.centerY.equalToSuperview()
+        view.addSubviews(appTitleLabel, regionListHorizontalView)
+        
+        appTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.equalToSuperview().inset(20)
         }
+        
+        regionListHorizontalView.snp.makeConstraints {
+            $0.top.equalTo(appTitleLabel.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(60)
+        }
+    }
+    
+    func setRegion(region: String) {
+        self.region = region
     }
 }
