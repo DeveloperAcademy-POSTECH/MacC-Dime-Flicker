7945987ccdcb7e9e48bf193f92847215e42c55fd
Jay <103009135+Rookie0031@users.noreply.github.com>
2022-11-17 22:50:57 +0900

    :bug: 이미지 통신 후 콜렉션뷰 업데이트 관련 에러 해결

--- Flicker/Screens/Artist/ArtistTappedViewController.swift
+++ Flicker/Screens/Artist/ArtistTappedViewController.swift
@@ -9,13 +9,13 @@ import Then
 import SnapKit
 
 final class ArtistTappedViewController: BaseViewController {
-
+    
     private let networkManager = NetworkManager.shared
-
+    
     private var imageList: [UIImage] = []
-
+    
     private var headerHeight: Int = 700
-
+    
     private lazy var portfolioFlowLayout = UICollectionViewFlowLayout().then {
         let imageWidth = (UIScreen.main.bounds.width - 50)/3
         $0.itemSize = CGSize(width: imageWidth , height: imageWidth)
@@ -23,16 +23,16 @@ final class ArtistTappedViewController: BaseViewController {
         $0.minimumInteritemSpacing = 5
         $0.headerReferenceSize = CGSize(width: UIScreen.main.bounds.width, height: CGFloat(headerHeight))
     }
-
+    
     private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: portfolioFlowLayout).then { $0.contentInsetAdjustmentBehavior = .never
     }
-
+    
     private let bottomBackgroundView = {
         let UIView = UIView()
         UIView.backgroundColor = .white
         return UIView
     }()
-
+    
     private let counselingButton = UIButton().then {
         $0.setTitle("문의하기", for: .normal)
         $0.titleLabel?.font = UIFont.preferredFont(forTextStyle: .callout, weight: .black)
@@ -40,13 +40,13 @@ final class ArtistTappedViewController: BaseViewController {
         $0.backgroundColor = .mainPink
         $0.layer.cornerRadius = 15
     }
-
+    
     private let mutualPayLabel = UILabel().makeBasicLabel(labelText: "상호 페이", textColor: .textSubBlack.withAlphaComponent(0.9), fontStyle: .title3, fontWeight: .bold)
-
+    
     private let statusBarBackGroundView = UIView().then {
         $0.backgroundColor = .white
     }
-
+    
     private let navigationBarSeperator = UIView().then {
         $0.backgroundColor = .systemGray5
     }
@@ -56,47 +56,43 @@ final class ArtistTappedViewController: BaseViewController {
     }
     
     override func viewDidLoad() {
-        collectionView.register(ArtistPortfolioCell.self, forCellWithReuseIdentifier: ArtistPortfolioCell.className)
-
-        collectionView.register(HeaderCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderCollectionReusableView.className)
-
+        
         setDelegateAndDataSource()
         
         view.addSubviews(collectionView, statusBarBackGroundView, navigationBarSeperator, bottomBackgroundView, counselingButton, bottomBarSeperator)
-
+        
+        collectionView.register(ArtistPortfolioCell.self, forCellWithReuseIdentifier: ArtistPortfolioCell.className)
+        
+        collectionView.register(HeaderCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderCollectionReusableView.className)
+        
         Task {
             await fetchPortfolioImages()
         }
-
+        
         configUI()
         setupBackButton()
         setupNavigationBar()
-
+        
     }
-
+    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         statusBarBackGroundView.isHidden = true
         navigationBarSeperator.isHidden = true
         navigationController?.navigationBar.backgroundColor = .clear
     }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        resetHeaderViewSize()
-    }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.navigationBar.backgroundColor = .clear
     }
-
+    
     override func configUI() {
         tabBarController?.tabBar.isHidden = true
         statusBarBackGroundView.isHidden = true
         navigationBarSeperator.isHidden = true
     }
-
+    
     override func setupNavigationBar() {
         guard let navigationBar = navigationController?.navigationBar else { return }
         let appearance = UINavigationBarAppearance()
@@ -104,7 +100,7 @@ final class ArtistTappedViewController: BaseViewController {
         navigationBar.standardAppearance = appearance
         navigationBar.compactAppearance = appearance
         navigationBar.scrollEdgeAppearance = appearance
-
+        
         //        //TODO: 공유하기 기능으로 출시 후 업데이트 예정
         //        let shareImageView = UIImageView().then {
         //            $0.image = UIImage(systemName: "square.and.arrow.up")
@@ -114,7 +110,7 @@ final class ArtistTappedViewController: BaseViewController {
         //        }
         //        navigationItem.rightBarButtonItem = makeBarButtonItem(with: shareImageView)
     }
-
+    
     //TODO: 출시 후, 앱스토어 링크 넣을 예정
     //    @objc private func didTapShare(_ sender: Any) {
     //        guard let image = UIImage(named: "AppIcon") else { return }
@@ -123,25 +119,25 @@ final class ArtistTappedViewController: BaseViewController {
     //        activityViewController.popoverPresentationController?.sourceView = self.view
     //        self.present(activityViewController, animated: true, completion: nil)
     //    }
-
+    
     private func setDelegateAndDataSource() {
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.showsVerticalScrollIndicator = false
     }
-
+    
     private func fetchPortfolioImages() async {
         do {
             self.imageList = try await networkManager.fetchImages(withURLs: networkManager.portFolioImageList)
-            DispatchQueue.main.async {
-                self.collectionView.reloadData()
+            self.collectionView.reloadData()
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1) {
+                self.resetHeaderViewSize()
             }
         } catch {
-            print("=====================")
             print(error)
         }
     }
-
+    
     private func resetHeaderViewSize() {
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.headerReferenceSize = CGSize(width: UIScreen.main.bounds.width, height: CGFloat(headerHeight))
@@ -159,36 +155,36 @@ final class ArtistTappedViewController: BaseViewController {
         
         // navigationBar 상단의 statusBar 자리를 UIView로 대체하여 조정
         statusBarBackGroundView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         statusBarBackGroundView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-
+        
         statusBarBackGroundView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-
+        
         statusBarBackGroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-
+        
         statusBarBackGroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-
+        
         statusBarBackGroundView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         navigationBarSeperator.snp.makeConstraints {
             $0.width.equalToSuperview()
             $0.height.equalTo(1)
         }
-
+        
         guard let navigationBar = navigationController?.navigationBar else { return }
-
+        
         navigationBarSeperator.topAnchor.constraint(equalTo: navigationBar.bottomAnchor).isActive = true
-
+        
         // 하단의 문의하기 버튼이 있는 UIView
         bottomBackgroundView.addSubviews(counselingButton, mutualPayLabel)
-
+        
         bottomBackgroundView.snp.makeConstraints {
             $0.bottom.equalToSuperview()
             $0.centerX.equalToSuperview()
             $0.height.equalTo(view.frame.height / 10)
             $0.width.equalToSuperview()
         }
-
+        
         counselingButton.snp.makeConstraints {
             $0.trailing.equalToSuperview().inset(20)
             $0.top.equalToSuperview().inset(10)
@@ -210,39 +206,40 @@ final class ArtistTappedViewController: BaseViewController {
 }
 
 extension ArtistTappedViewController: UICollectionViewDataSource {
-
+    
     // numberOfCell
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return imageList.count
     }
-
+    
     // ReusableCell setting + image loading
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath)
     -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ArtistPortfolioCell.className, for: indexPath) as! ArtistPortfolioCell
-
         // 이미지 URL을 가진 response 배열
         let image = imageList[indexPath.item]
         cell.image = image
         return cell
     }
-
+    
     // dequeheaderView, set headerHeight
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: HeaderCollectionReusableView.className, for: indexPath) as! HeaderCollectionReusableView
         headerHeight = Int(headerView.getTotalViewHeight())
-
         headerView.resetPortfolioImage(with: imageList)
         return headerView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didEndDisplayingSupplementaryView view: UICollectionReusableView, forElementOfKind elementKind: String, at indexPath: IndexPath) {
     }
 }
 
 extension ArtistTappedViewController: UICollectionViewDelegate {
-
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-
+        
         let cell = collectionView.cellForItem(at: indexPath) as! ArtistPortfolioCell
-
+        
         let viewController = ImageViewController()
         viewController.image = cell.imageView.image
         viewController.modalPresentationStyle = .fullScreen
@@ -252,14 +249,14 @@ extension ArtistTappedViewController: UICollectionViewDelegate {
         }
         present(viewController, animated: false)
     }
-
+    
     // 스크롤시 네비게이션바 커스텀화
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if collectionView.contentOffset.y > 280 {
             navigationController?.navigationBar.backgroundColor = .white
             statusBarBackGroundView.isHidden = false
             navigationBarSeperator.isHidden = false
-
+            
         } else {
             navigationController?.navigationBar.backgroundColor = .clear
             statusBarBackGroundView.isHidden = true
@@ -270,7 +267,7 @@ extension ArtistTappedViewController: UICollectionViewDelegate {
 
 
 extension ArtistTappedViewController: UICollectionViewDelegateFlowLayout {
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
     }
