7d3ab62b5b345830b52975b3a4b08b2056455958
Hardydev <dusgh15378@gmail.com>
2022-11-21 17:57:31 +0900

    ✨ Add deleteUserDefault

--- Flicker/Screens/Profile/ProfileViewController.swift
+++ Flicker/Screens/Profile/ProfileViewController.swift
@@ -96,6 +96,7 @@ final class ProfileViewController: BaseViewController {
         makeRequestAlert(title: "로그아웃 하시겠어요?", message: "", okAction: { _ in
             Task {
                 [weak self] in
+                await CurrentUserDataManager.shared.deleteUserDefault()
                 await LoginManager.shared.fireBaseSignOut()
                 DispatchQueue.main.async {
                     self?.goLogin()
@@ -107,6 +108,7 @@ final class ProfileViewController: BaseViewController {
     private func doSignOut() {
         makeRequestAlert(title: "정말 탈퇴하시겠어요?", message: "회원님의 가입정보는 즉시 삭제되며, 복구가 불가능합니다.", okAction: { _ in
             Task { [weak self] in
+                await CurrentUserDataManager.shared.deleteUserDefault()
                 await LoginManager.shared.appleLoginReAuthUser()
                 await LoginManager.shared.fireBasewithDraw()
                 self?.navigationController?
