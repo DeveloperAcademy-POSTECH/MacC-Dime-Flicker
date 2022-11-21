afef30b0ee6a46f0ad1b2fc2ae4a76f5bd056366
Rookie0031 <jisu7408@gmail.com>
2022-11-21 19:34:30 +0900

    :art: 통신관련 코드 에러처리 개선

--- Flicker/Network/NetworkManager.swift
+++ Flicker/Network/NetworkManager.swift
@@ -29,31 +29,42 @@ class NetworkManager {
 
     func fetchOneImage(withURL url: String) async throws -> UIImage {
         var image = UIImage()
-        if let cachedImage = cachedImages.object(forKey: url as NSString) {
-            print("cached Image available")
-            image = UIImage(data: cachedImage as Data) ?? UIImage()
-        } else {
-            guard let imageRequestURL = URL(string: url) else { throw NetworkError.badLocalUrl }
-            async let (data, urlResponse) = URLSession.shared.data(from: imageRequestURL)
-            try await self.cachedImages.setObject(data as NSData, forKey: url as NSString)
-            let httpResponse = try await urlResponse as! HTTPURLResponse
-            if !(200...299).contains(httpResponse.statusCode) {
-                throw NetworkError.responseError
+        do {
+            if let cachedImage = cachedImages.object(forKey: url as NSString) {
+                print("cached Image available")
+                image = UIImage(data: cachedImage as Data) ?? UIImage()
+            } else {
+                guard let imageRequestURL = URL(string: url) else { throw NetworkError.badLocalUrl }
+                async let (data, urlResponse) = URLSession.shared.data(from: imageRequestURL)
+                try await self.cachedImages.setObject(data as NSData, forKey: url as NSString)
+                let httpResponse = try await urlResponse as! HTTPURLResponse
+                if !(200...299).contains(httpResponse.statusCode) {
+                    throw NetworkError.responseError
+                }
+                guard let responseImage = try await UIImage(data: data) else { throw NetworkError.responseError }
+                image = responseImage
             }
-            guard let responseImage = try await UIImage(data: data) else { throw NetworkError.responseError }
-            image = responseImage
+        } catch NetworkError.responseError {
+            print(NetworkError.responseError.localizedDescription)
+        } catch NetworkError.badLocalUrl {
+            print(NetworkError.badLocalUrl.localizedDescription)
         }
         return image
     }
 
     func fetchImages(withURLs urls: [String]) async throws -> [UIImage] {
         var images: [UIImage] = []
-
-        for url in urls {
-            let image: UIImage = try await self.fetchOneImage(withURL: url)
-            if !images.contains(image) {
-                images.append(image)
+        do {
+            for url in urls {
+                let image: UIImage = try await self.fetchOneImage(withURL: url)
+                if !images.contains(image) {
+                    images.append(image)
+                }
             }
+        } catch NetworkError.responseError {
+            print(NetworkError.responseError.localizedDescription)
+        } catch NetworkError.badLocalUrl {
+            print(NetworkError.badLocalUrl.localizedDescription)
         }
         return images
     }
