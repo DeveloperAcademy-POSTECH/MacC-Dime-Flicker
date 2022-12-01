2bfd5176d72d2062e50db93732811f8e0b0d3acb
Jay <103009135+Rookie0031@users.noreply.github.com>
2022-12-01 22:34:51 +0900

    :sparkles: 이미지 확대 축소 기능 추가

--- Flicker/Screens/Artist/ImageViewController.swift
+++ Flicker/Screens/Artist/ImageViewController.swift
@@ -10,45 +10,92 @@ import SnapKit
 import Then
 
 final class ImageViewController: UIViewController {
-
+    
     private let imageView = UIImageView().then {
         $0.contentMode = .scaleAspectFit
         $0.clipsToBounds = true
+        $0.isUserInteractionEnabled = true
     }
-
+    
+    private lazy var scrollView = UIScrollView(frame: .zero).then {
+        $0.showsVerticalScrollIndicator = false
+        $0.delegate = self
+        $0.zoomScale = 1.0
+        $0.minimumZoomScale = 1.0
+        $0.maximumZoomScale = 3.0
+    }
+    
     var image: UIImage? = UIImage(named: "port1")
-
+    
     private let cancelImageView = UIImageView().then {
         $0.isUserInteractionEnabled = true
         $0.tintColor = .white
         $0.contentMode = .scaleAspectFill
         $0.image = UIImage(systemName: "x.circle.fill")
     }
-
+    
     var completion: ()->Void = {}
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubviews(imageView, cancelImageView)
+        view.addSubviews(scrollView, cancelImageView)
+        scrollView.addSubview(imageView)
         view.backgroundColor = .black
         imageView.image = image
-        cancelImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapCancelButton(_:))))
+        addGestures()
         render()
     }
-
+    
     private func render() {
-        imageView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+        scrollView.snp.makeConstraints {
+            $0.width.height.equalToSuperview()
         }
 
+        imageView.snp.makeConstraints {
+            $0.width.height.equalToSuperview()
+        }
+        
         cancelImageView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide).inset(10)
             $0.width.height.equalTo(28)
             $0.trailing.equalToSuperview().inset(20)
         }
     }
-
+    
+    private func addGestures() {
+        cancelImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapCancelButton(_:))))
+    }
+    
     @objc private func didTapCancelButton(_ sender: Any) {
         dismiss(animated: false) { self.completion() }
+    }
+}
+
+extension ImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        if scrollView.zoomScale > 1 {
+            if let image = imageView.image {
+                let widthRatio = imageView.frame.width / image.size.width
+                let heightRatio = imageView.frame.height / image.size.height
+                
+                let ratio = widthRatio < heightRatio ? widthRatio : heightRatio
+                let newWidth = image.size.width * ratio
+                let newHeight = image.size.height * ratio
+                let conditionLeft = newWidth * scrollView.zoomScale > imageView.frame.width
+                let left = 0.5 * (conditionLeft ? newWidth - imageView.frame.width : (scrollView.frame.width - scrollView.contentSize.width))
+                let conditioTop = newHeight * scrollView.zoomScale > imageView.frame.height
+                
+                let top = 0.5 * (conditioTop ? newHeight - imageView.frame.height : (scrollView.frame.height - scrollView.contentSize.height))
+                
+                scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: top, right: left)
+                
+            }
+        } else {
+            scrollView.contentInset = .zero
+        }
     }
 }
