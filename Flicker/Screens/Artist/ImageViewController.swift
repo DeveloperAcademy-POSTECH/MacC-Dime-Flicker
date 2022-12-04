eec2527878c28c80cdbc9e15dc52c0b9c25550d0
Rookie0031 <jisu7408@gmail.com>
2022-12-04 23:47:55 +0900

    :sparkles: 이미지 더블 탭 시 확대 및 원래 사이즈로 복귀 기능 추가

--- Flicker/Screens/Artist/ImageViewController.swift
+++ Flicker/Screens/Artist/ImageViewController.swift
@@ -77,8 +77,31 @@ final class ImageViewController: UIViewController {
         dismiss(animated: false) { self.completion() }
     }
 
-    @objc private func didDoubleTapImage(_ sender: Any) {
-        self.scrollView.zoomScale = 1
+    @objc private func didDoubleTapImage(_ sender: UITapGestureRecognizer) {
+        let scale = min(scrollView.zoomScale * 2, scrollView.maximumZoomScale)
+
+        if scale != scrollView.zoomScale { // zoom in
+            let point = sender.location(in: imageView)
+
+            let scrollSize = scrollView.frame.size
+            let size = CGSize(width: scrollSize.width / scrollView.maximumZoomScale,
+                              height: scrollSize.height / scrollView.maximumZoomScale)
+            let origin = CGPoint(x: point.x - size.width / 2,
+                                 y: point.y - size.height / 2)
+            scrollView.zoom(to:CGRect(origin: origin, size: size), animated: true)
+        } else if scrollView.zoomScale > 1 {
+            scrollView.zoom(to: zoomRectForScale(scale: 1, center: sender.location(in: imageView)), animated: true)
+        }
+    }
+
+    func zoomRectForScale(scale: CGFloat, center: CGPoint) -> CGRect {
+        var zoomRect = CGRect.zero
+        zoomRect.size.height = imageView.frame.size.height / scale
+        zoomRect.size.width  = imageView.frame.size.width  / scale
+        let newCenter = scrollView.convert(center, from: imageView)
+        zoomRect.origin.x = newCenter.x - (zoomRect.size.width / 2.0)
+        zoomRect.origin.y = newCenter.y - (zoomRect.size.height / 2.0)
+        return zoomRect
     }
 }
 
